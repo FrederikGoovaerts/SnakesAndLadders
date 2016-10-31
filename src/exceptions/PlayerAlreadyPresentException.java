2675b37b336fd44232bde51d432f4f0bb9e6914f
Frederik Goovaerts <FrederikGoovaerts@users.noreply.github.com>
2016-10-31 13:17:16 +0100

    Updated exception documentation

--- src/exceptions/PlayerAlreadyPresentException.java
+++ src/exceptions/PlayerAlreadyPresentException.java
@@ -3,10 +3,13 @@ package exceptions;
 public class PlayerAlreadyPresentException extends SnakesAndLaddersException {
 
 	/**
-	 * 
+	 * The serial UID for this exception
 	 */
 	private static final long serialVersionUID = -1807228140921985063L;
 
+	/**
+	 * This exception is thrown when a player is tried to be added when a player with the same name is present
+	 */
 	public PlayerAlreadyPresentException(String string) {
 		super(string);
 	}
