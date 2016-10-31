59b6383235861b9f32fe76cd4e8182a5ed333099
Frederik Goovaerts <FrederikGoovaerts@users.noreply.github.com>
2016-10-31 13:13:31 +0100

    Document exception

--- src/exceptions/NoPlayersPresentException.java
+++ src/exceptions/NoPlayersPresentException.java
@@ -7,6 +7,9 @@ public class NoPlayersPresentException extends SnakesAndLaddersException {
 	 */
 	private static final long serialVersionUID = 5352520326677480180L;
 
+	/**
+	 * Exception thrown when game is to be started when no players are present
+	 */
 	public NoPlayersPresentException(String string) {
 		super(string);
 	}
